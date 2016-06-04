f2bccf95c78299c427d47da7e0fcba7ea78d7b6c
Vinícius Cardoso Austria <viniciusaustria@gmail.com>
2016-06-04 09:47:48 -0300

    Update ruby.rb
    added recursive method

--- ruby/ruby.rb
+++ ruby/ruby.rb
@@ -5,3 +5,10 @@ def fib(numbers, count:)
 end
 
 puts fib([0,1], count: 15).join(', ')
+
+
+def fibo_simplified( number )
+  number <= 1 ? number :  fibo_simplified( number - 1 ) + fibo_simplified( number - 2 )
+end
+
+puts fibo_simplified( 15 )
